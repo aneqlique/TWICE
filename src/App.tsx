e84f3ac666442f3b96c3f2144cf91e88fea60f50
Angelique Anne Valdez <iamxngelique@gmail.com>
2025-06-30 18:07:58 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@
 export default function App() {
   return (
     <>
-      <p className="text-2xl">one in a million!</p>       
+      <p className="text-2xl">one in a million!</p>   
+
+      <body className="bg-(--color-white) scroll-smooth">
+        <main>
+          
+          <div className="bg-[url(/images/background.png)] rounded-bl-[50px] rounded-br-[50px] rounded-tl-none rounded-tr-none relative h-197 bg-cover bg-center flex items-center justify-center">
+          
+          </div>
+
+          
+        </main>
+      </body>
+
+
     </>
   )
 }
